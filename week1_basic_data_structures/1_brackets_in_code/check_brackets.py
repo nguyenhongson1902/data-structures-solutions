565000fd189a0074d6722af34027c29e995361ef
Nguyen Hong Son <nguyenhongsonk62hust@gmail.com>
2022-10-12 00:20:26 +0700

    Add comments to explain code at line 5, 22, 31, 34, and 36

--- week1_basic_data_structures/1_brackets_in_code/check_brackets.py
+++ week1_basic_data_structures/1_brackets_in_code/check_brackets.py
@@ -2,7 +2,7 @@
 
 from collections import namedtuple
 
-Bracket = namedtuple("Bracket", ["char", "position"])
+Bracket = namedtuple("Bracket", ["char", "position"]) # I don't know what's this line for
 
 
 def are_matching(left, right):
@@ -14,18 +14,34 @@ def find_mismatch(text):
     for i, next in enumerate(text):
         if next in "([{":
             # Process opening bracket, write your code here
-            pass
+            opening_brackets_stack.append(next)
+            opening_pos = i + 1
 
         if next in ")]}":
             # Process closing bracket, write your code here
-            pass
+            if not opening_brackets_stack: # if stack is empty
+                return i + 1
+            top = opening_brackets_stack.pop()
+            
+            if not are_matching(top, next):
+                return i + 1
+            
+            opening_pos -= 1
+    
+    if not opening_brackets_stack: # if stack is empty
+        return 'Success'
+    elif opening_pos >= 1:
+        return opening_pos - len(opening_brackets_stack) + 1 # find the first index of the unmatched opening bracket
+    else:
+        return i + 1 # The last position
+            
 
 
 def main():
     text = input()
     mismatch = find_mismatch(text)
     # Printing answer, write your code here
-
+    print(mismatch)
 
 if __name__ == "__main__":
     main()
