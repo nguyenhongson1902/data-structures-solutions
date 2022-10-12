91ac212a94d875b5f5707c9d4dfecbddd11e0c64
Nguyen Hong Son <nguyenhongsonk62hust@gmail.com>
2022-10-13 01:44:04 +0700

    Add a link to solution with the 3rd hint

--- week1_basic_data_structures/5_max_sliding_window/max_sliding_window.py
+++ week1_basic_data_structures/5_max_sliding_window/max_sliding_window.py
@@ -1,11 +1,38 @@
 # python3
+from collections import deque
 
 
-def max_sliding_window_naive(sequence, m):
+
+# naive solution
+# def max_sliding_window_naive(sequence, m):
+#     maximums = []
+#     for i in range(len(sequence) - m + 1):
+#         maximums.append(max(sequence[i:i + m]))
+
+#     return maximums
+
+def max_sliding_window(sequence, m):
+    """
+    Reference: https://www.youtube.com/watch?v=DfljaUwZsOk&ab_channel=NeetCode
+    """
     maximums = []
-    for i in range(len(sequence) - m + 1):
-        maximums.append(max(sequence[i:i + m]))
+    d = deque([])
+    l = r = 0
 
+    while r < len(sequence):
+        while d and sequence[d[-1]] < sequence[r]:
+            d.pop()
+        d.append(r)
+
+        if l > d[0]:
+            d.popleft()
+        
+        if r + 1 >= m:
+            maximums.append(sequence[d[0]])
+            l += 1
+        
+        r += 1
+    
     return maximums
 
 if __name__ == '__main__':
@@ -14,5 +41,5 @@ if __name__ == '__main__':
     assert len(input_sequence) == n
     window_size = int(input())
 
-    print(*max_sliding_window_naive(input_sequence, window_size))
+    print(*max_sliding_window(input_sequence, window_size))
 
